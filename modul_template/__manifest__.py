3efa292e181e2ef9275a7bbed2c832a2512749c1
Mazmur Tarigan <52620289+maztarigan@users.noreply.github.com>
2023-07-24 05:18:45 +0700

    Add files via upload
    Modul template ini digunakan sebagai master untuk memudahkan setiap hendak membuat modul baru.

---
+++ modul_template/__manifest__.py
@@ -0,0 +1,23 @@
+{
+  'name': 'Nama Business App / Module',
+  'author': 'Jayanusa',
+  'version': '0.1',
+  'depends': [
+    # 'nama_modul',
+  ],
+  'data': [
+    # 'views/nama_file.xml',
+  ],
+  'qweb': [
+    # 'static/src/xml/nama_widget.xml',
+  ],
+  'sequence': 1,
+  'auto_install': False,
+  'installable': True,
+  'application': True,
+  'category': '- Bootcamp Batch 1',
+  'summary': 'Catatan Modul',
+  'license': 'OPL-1',
+  'website': 'https://www.jayanusa.ac.id/',
+  'description': '-'
+}
